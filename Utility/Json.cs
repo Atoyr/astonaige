12d3e00bc8b72801456a4eefd377970e98a9346b
Atoyr <lessironglance@gmail.com>
2017-03-08 15:16:30 +0900

    System.Runtime.Serialization.Jsonの追加
    NuGetからSystem.Runtime.Serialization.Jsonを追加

--- Utility/Json.cs
+++ Utility/Json.cs
@@ -1,3 +1,29 @@
-namespace astonaige.Utility{
-    
+using System;
+using System.Text;
+using System.IO;
+using System.Runtime.Serialization.Json;
+
+namespace astonaige.Utility
+{
+    public static class Json
+    {
+        public static string SerializeJson<T>(T obj) where T : class
+        {
+            var serializer = new DataContractJsonSerializer(typeof(T));
+            using (var stream = new MemoryStream())
+            {
+                serializer.WriteObject(stream, obj);
+                return Encoding.UTF8.GetString(stream.ToArray());
+            }
+        }
+
+        public static T DeserializeJson<T>(string json) where T : class
+        {
+            using (var stream = new MemoryStream(Encoding.UTF8.GetBytes(json)))
+            {
+                var serializer = new DataContractJsonSerializer(typeof(T));
+                return (T)serializer.ReadObject(stream);
+            }
+        }
+    }
 }
